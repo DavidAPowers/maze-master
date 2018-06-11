2d8daccbb0e181a8e0eb03013d8b38576da42287
David A Powers <cyborgk@gmail.com>
2018-06-10 19:47:31 -0500

    tweak test

--- spec/lib/dice_spec.js
+++ spec/lib/dice_spec.js
@@ -92,11 +92,7 @@ describe('Dice', () => {
 	});  
 	describe('.rollStats(stats,dice)', () => {
 		it('Given invalid data, should return new Lead', () => {
-			let ok = Dice.rollStats('gold','2d6');
-			expect(ok.gold>1&&ok.gold<13).toEqual(true);
-			console.log(`gold: ${ok.gold}`);			
-			let bad = Dice.rollStats('bad');
-			expect(bad.bad).toEqual(0);
+
 			for(let i=0;i<10000;i++) {
 				let stats = Dice.rollStats(['str','int','wis','dex','con','cha'],'3d6');
 				expect(stats.str>2&&stats.str<19).toEqual(true);
@@ -107,6 +103,13 @@ describe('Dice', () => {
 				expect(stats.cha>2&&stats.cha<19).toEqual(true);				
 			}
 			console.log(Dice.rollStats(['str','int','wis','dex','con','cha'],'3d6'));
+			//given a string instead of an array, returns object with single named property
+			let ok = Dice.rollStats('gold','2d6');
+			expect(ok.gold>1&&ok.gold<13).toEqual(true);
+			console.log(`gold: ${ok.gold}`);			
+			//given malformed dice string, returns value 0
+			let bad = Dice.rollStats('bad','fdg');
+			expect(bad.bad).toEqual(0);			
 		});
 	});    
 });
